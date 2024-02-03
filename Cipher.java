d5d31eb8ba1ccacbc4ba042afcd8f439cc3c4145
XchraQeLymePi <69987770+XchraQeLymePi@users.noreply.github.com>
2024-02-02 19:30:25 -0800

    Update Cipher.java

--- Cipher.java
+++ Cipher.java
@@ -10,7 +10,7 @@
  public class Cipher {
      private static Scanner scanner = new Scanner(System.in);
      private char[] characters; // supported characters in specified order
-     private String original;
+     public String original; //make private later, and add a get/set function
      // TODO: Declare more private attributes here
  
      /* precondition: 0 <= offset < 60, 1 <= blockSize */
@@ -111,69 +111,6 @@
          return bestDeciphered;
      }
  
-     public static void main(String[] args)
-     {
-         System.out.println("Welcome to Cipher! Please enter all text as a single line, substituting \\n for newline characters.");
-         boolean done = false;
-         String command;
-         Cipher mainCipher = new Cipher("");
-         while (!done) {
-             while (true){
-                 System.out.print("Would you like to encipher, decipher, crack, or quit? ");
-                 command = scanner.nextLine();
-                 if (command.equals("encipher")){
-                     System.out.println("Enter your string");
-                     String input = scanner.nextLine();
-                     mainCipher.original = input;
-                     mainCipher.encipher();
-                 }
-                 else if (command.equals("decipher")){
-                     System.out.println("Enter your string");
-                     String input = scanner.nextLine();
-                     mainCipher.original = input;
-                     mainCipher.decipher();
-                 }
-                 else if (command.equals("crack")){
-                     System.out.println("Enter your string");
-                     String input = scanner.nextLine();
-                     mainCipher.original = input;
-                     mainCipher.crack();
-                     break;
-                 }
-                 else if (command.equals("offset")){
-                    System.out.println("Enter your string");
-                    String input = scanner.nextLine();
-                    System.out.println("Enter your offset");
-                    String offset = scanner.nextLine();
-                    mainCipher.original = input;
-                    System.out.println(mainCipher.offset(Integer.parseInt(offset)));
-                    break;
-                }
-                else if (command.equals("blockReverse")){
-                    System.out.println("Enter your string");
-                    String input = scanner.nextLine();
-                    System.out.println("Enter your blockSize");
-                    String blockSize = scanner.nextLine();
-                    mainCipher.original = input;
-                    System.out.println(mainCipher.blockReverse(Integer.parseInt(blockSize)));
-                    break;
-                }
-                 else if (command.equals("quit")){
-                     done = true;
-                     break;
-                 }
-             }
-             /* TODO: Add code to check which command the user entered, prompt for
-              * additional information as needed (offset, blocksize, text for encipher and decipher,
-              * text for crack), call other methods to perform the selected operation, and print the result.
-              * When the user enters quit as the command, just set variable done to true.
-              * Since both clear and encrypted text may contain newline characters, allow users to enter the
-              * two character sequence \n and replace those with actual newline characters before calling your
-              * methods.  The testoutputs.txt file assumes you will leave newline characters alone when
-              * printing output.
-              */
- 
-         }
-     }
+     
  }
  

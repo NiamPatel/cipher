8fff8d4d0a9d0b94b90ea5ce281cbdef944b3e1e
XchraQeLymePi <69987770+XchraQeLymePi@users.noreply.github.com>
2024-02-04 21:53:26 -0800

    Update Cipher.java

--- Cipher.java
+++ Cipher.java
@@ -1,64 +1,57 @@
 import java.io.BufferedWriter;
 import java.io.FileWriter;
 import java.io.IOException;
- 
- public class Cipher {
-     public String getoriginal()
-     {
-     return original;
-     }
-     public void setoriginal(String new_string)
-     {
-     this.original = new_string;
-     }
-     private char[] characters; // supported characters in specified order
-     private String original; //make private later, and add a get/set function
-     // TODO: Declare more private attributes here
- 
-     /* precondition: 0 <= offset < 60, 1 <= blockSize */
- 
-     /**
-      * Cipher constructor
-      * precondition: 0 <= offset < 60, 1 <= blockSize
-      * @param offset offset to index in characters array
-      * @param blockSize size of blocks to reverse
-      */
-     public Cipher(String input) {
-         characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz \n,;:.!?".toCharArray();
-         this.original = input;
-     }
- 
- 
- 
-     /**
-      * The encipher method returns the result of enciphering the given clear text string 
-      * @param s clear text string to encipher
-      * @return enciphered string
-      */
- 
-     public String offset(int displacement, String new_string){ //change to private later
-        if (displacement < 0){
-            displacement = displacement%60 + 60;
+
+public class Cipher {
+    // try using lambac
+    private char[] characters; // supported characters in specified order 
+    private String original; // make private later, and add a get/set function
+    private AllWords allwords = new AllWords("All_Words.txt");
+
+    public String getoriginal() {
+        return original;
+    }
+
+    public void setoriginal(String new_string) {
+        this.original = new_string;
+    }
+
+    public Cipher(String input) {
+        characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz \n,;:.!?".toCharArray();
+        this.original = input;
+    }
+
+    /**
+     * The encipher method returns the result of enciphering the given clear text
+     * string
+     * 
+     * @param s clear text string to encipher
+     * @return enciphered string
+     */
+
+    public String offset(int displacement, String newString) { // change to private later
+
+        if (displacement < 0) {
+            displacement = displacement % 60 + 60;
         }
-        //copy array
-         char[] stringArr = new_string.toCharArray();
-         char[] modifiedArr = new char[stringArr.length];
+        // copy array
+        //char[] stringArr = newString.toCharArray();
+        final int len = newString.length();
+        char[] modifiedArr = new char[len];
 
-         for (int i = 0; i < stringArr.length; i++){
-             modifiedArr[i] = stringArr[(i+displacement)%(stringArr.length)];
-         }
+        for (int i = 0; i < len; i++) {
+            modifiedArr[i] = newString.charAt((i + displacement) % (len));
+            //modifiedArr[i] = stringArr[(i + displacement) % (stringArr.length)];
+        }
+        
+        return new String(modifiedArr);
+    }
 
-         String result = new String(modifiedArr);
-         return result;
-     }
- 
-     public String blockReverse(int blockSize, String new_string) {
-        char[] stringArr = new_string.toCharArray();
+    public String blockReverse(int blockSize, String newString) { // change method name to reverseBlock
+        char[] stringArr = newString.toCharArray();
         char[] modifiedArr = new char[stringArr.length];
-        
-        
-    
-        for (int i = 0; i < Math.ceil((double)stringArr.length / blockSize); i++) {
+
+        for (int i = 0; i < Math.ceil((double) stringArr.length / blockSize); i++) {
             // Calculate the start and end indices for the current block
             int start = i * blockSize;
             int end = Math.min((i + 1) * blockSize, stringArr.length) - 1;
@@ -70,52 +63,66 @@ import java.io.IOException;
         String result = new String(modifiedArr);
         return result;
     }
-    
-     public String cipher(int displacement, int blockSize, int order) {
-         String modified = "";
-         if (order != 0){
+
+    public String cipher(int displacement, int blockSize, int order) {
+        String modified = "";
+        if (order != 0) {
             modified = offset(displacement, this.original);
             modified = blockReverse(blockSize, modified);
-         } else {
+        } else {
             modified = blockReverse(blockSize, this.original);
             modified = offset(displacement, modified);
-         }
- 
-         return modified;
-     }
+        }
 
-     
-     public String crack()
-     {
-        try (BufferedWriter writer = new BufferedWriter(new FileWriter("all_crack.txt"))) {
+        return modified;
+    }
+
+    private int matchDictionary(String[] stringArr) {
+        int score = 0;
+        for (String word : stringArr) {
+            if (allwords.getStringHashSet().contains(word.toLowerCase())) {
+                score++;
+            }
+        }
+        return score;
+    }
+
+    public String crack() {
+        try (var writer = new BufferedWriter(new FileWriter("all_crack.txt"))) {
             // Write an empty string to clear the file
             writer.write("");
 
             System.out.println("Successfully cleared the file.");
         } catch (IOException e) {
-            System.err.println("Error clearing the file: " + e.getMessage());
+            System.err.println("Error clearing the file: "
+            
+                + e.getMessage());
         }
-         //System.out.println(this.original);
-         String bestDeciphered = null;
-         
-         for (int i = 0; i <= 1; i++){
-            for (int j = 0; j < this.original.length(); j++){
-                for (int k = 1; k < this.original.length(); k++){
-                    try (BufferedWriter writer = new BufferedWriter(new FileWriter("all_crack.txt", true))) {
+        // System.out.println(this.original);
+        String bestDeciphered = null;
+        int bestScore = -1;
+        int score = 0;
+
+        for (int i = 0; i <= 1; i++) {
+            for (int j = 0; j < this.original.length(); j++) {
+                for (int k = 1; k < this.original.length(); k++) {
+                    try (var writer = new BufferedWriter(new FileWriter("all_crack.txt", true))) {
                         // Write the content to the file
-                        writer.write(cipher(j, k, i));
+                        String testcase = cipher(j, k, i);
+                        String[] testcaseArr = testcase.split(" ");
+                        score = matchDictionary(testcaseArr);
+                        if (score >= bestScore) {
+                            bestScore = score;
+                            bestDeciphered = testcase;
+                        }
+                        writer.write(testcase + score);
                         writer.newLine();
                     } catch (IOException e) {
                         System.err.println("Error writing to the file: " + e.getMessage());
                     }
-                }
+                } // end for reverseBlock combination
             }
-
-         }
-         
-         return bestDeciphered;
-     }
- 
-     
- }
- 
+        }
+        return bestDeciphered;
+    }
+}

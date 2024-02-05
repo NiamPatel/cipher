ea1c392994b1fa22cfac681fef84ba14e6b608a9
XchraQeLymePi <69987770+XchraQeLymePi@users.noreply.github.com>
2024-02-04 21:55:27 -0800

    Update Input.java

--- Input.java
+++ Input.java
@@ -15,7 +15,6 @@ public class Input {
             while (true) {
                 System.out.print("Would you like to (en/de)cipher, crack, or quit? ");
                 command = scanner.nextLine();
-
                 if (command.equalsIgnoreCase("cipher")) {
                     boolean validInput = false;
                     int displacement = 0, blockSize = 0, order = 0;
@@ -29,7 +28,7 @@ public class Input {
                             order = Integer.parseInt(parameterArr[2]);        // has to be int
                             
                             // Check for negative values and blockSize within the range
-                            if (displacement < 0 || blockSize < 1 || blockSize > input.length() || order < 0) {
+                            if (blockSize < 1 || blockSize > input.length()) {
                                 System.out.println("Please ensure displacement and order are non-negative and blockSize is between 1 and the length of the input string.");
                             } else {
                                 validInput = true;
@@ -41,7 +40,7 @@ public class Input {
                     System.out.println(mainCipher.cipher(displacement, blockSize, order));
                     break;
                 } else if (command.equalsIgnoreCase("crack")) {
-                    mainCipher.crack();
+                    System.out.println(mainCipher.crack());
                     break;
                 } else if (command.equalsIgnoreCase("quit")) {
                     done = true;
@@ -53,15 +52,3 @@ public class Input {
     }
 }
 
-            /* TODO: Add code to check which command the user entered, prompt for
-             * additional information as needed (offset, blocksize, text for encipher and decipher,
-             * text for crack), call other methods to perform the selected operation, and print the result.
-             * When the user enters quit as the command, just set variable done to true.
-             * Since both clear and encrypted text may contain newline characters, allow users to enter the
-             * two character sequence \n and replace those with actual newline characters before calling your
-             * methods. The testoutputs.txt file assumes you will leave newline characters alone when
-             * printing output.
-             */
-        }
-    }
-}

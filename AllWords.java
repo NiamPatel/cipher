e6cdad54caf5eda86502aedf20d40c8625e932ee
XchraQeLymePi <69987770+XchraQeLymePi@users.noreply.github.com>
2024-02-12 18:45:35 -0800

    Update AllWords.java

--- AllWords.java
+++ AllWords.java
@@ -1,4 +1,4 @@
-import java.io.IOException;//imports
+import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.HashSet;
@@ -8,14 +8,14 @@ import java.util.stream.Collectors;
 
 public class AllWords {
 
-    private List<String> allWords;//initaliztion 
+    private List<String> allWords;
     private Set<String> stringHashSet;
 
     public AllWords(String filePath) {
         // Read words from an external file
         try {
             allWords = Files.lines(Paths.get(filePath))
-                            .collect(Collectors.toList());//collectors
+                            .collect(Collectors.toList());
         } catch (IOException e) {
             e.printStackTrace(); // Handle the exception according to your needs
         }
@@ -25,12 +25,12 @@ public class AllWords {
 
         // Convert all words to lowercase
         stringHashSet = stringHashSet.stream()
-                                     .map(String::toLowerCase)//made them all lowercase 
+                                     .map(String::toLowerCase)
                                      .collect(Collectors.toSet());
     }
 
     // Getter method for stringHashSet
     public Set<String> getStringHashSet() {
-        return stringHashSet;//returns the string of the has set
+        return stringHashSet;
     }
 }
